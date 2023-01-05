74d5686cd218a7f76c6fd1fc68f85081c37a0856
Manasa <ybmanasa6@gmail.com>
2023-01-05 17:35:34 -0500

    First commit

---
+++ src/Day1/JavaConcepts.java
@@ -0,0 +1,23 @@
+package Day1;
+
+public class JavaConcepts {
+	
+	public static void main(String[] args)
+	{
+	int a = 10, b=20 ,c = 0;
+			
+	
+	System.out.println("The value of a:" + a + ','  +"The value of b:" + b);
+	
+	//c = a;
+	
+	//a = b;
+	
+	//b = c;
+	
+	a= a+10;
+	b = b-10;
+	System.out.println("The value of a:" + a + ','  +"The value of b:" + b);
+	
+	}
+}
